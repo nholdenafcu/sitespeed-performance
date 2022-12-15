e0b19cdcdade8c50263e300f2954582df4290fdd
Nathan R. Holden <NHolden@corp.additionfi.com>
2022-12-15 14:39:17 -0500

    testing for windows

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOCKER_CONTAINER=sitespeedio/sitespeed.io:26.1.0
-DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 "
+DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd):/sitespeed.io" -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 "
 DESKTOP_BROWSERS_DOCKER=(chrome firefox)
 DESKTOP_BROWSERS=(chrome firefox edge)
 EMULATED_MOBILE_BROWSERS=(chrome)
