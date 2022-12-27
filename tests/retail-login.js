d39332c155d3bfa15efd0210535a6a49439cd894
Nathan R. Holden <NHolden@corp.additionfi.com>
2022-12-27 15:32:09 -0500

    login change

--- tests/retail-login.js
+++ tests/retail-login.js
@@ -1,13 +1,13 @@
 module.exports = async function(context, commands) {
   await commands.navigate(
-    'https://web.tst.afcu.live.backbaseservices.com/retail-banking'
+    'https://web.stg.afcu.live.backbaseservices.com/retail-banking'
   );
 
   await commands.measure.start('login');
 
   try {
-    await commands.addText.byId('TestOJohnson', 'username');
-    await commands.addText.byId('BB21@Ojohnson', 'password-field');
+    await commands.addText.byId('TestAMonroe', 'username');
+    await commands.addText.byId('BB21@Amonroe', 'password-field');
 
     await commands.click.byIdAndWait('kc-login');
 
